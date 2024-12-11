4049a3a6f5607715ddf17224b2205523b99308dc
TrapFish <92753280+TrapFish@users.noreply.github.com>
2024-12-11 15:55:14 +0530

    middle ware mplementation

--- src/app.js
+++ src/app.js
@@ -2,7 +2,26 @@ const express = require('express');
 const app = express(); 
 const PORT = process.PORT || 3000;
 
+const {authAdmin} = require("./middlewares/auth.js");
 
+
+app.use('/admin', authAdmin);
+
+app.get("/user", (req, res) => {
+    res.send("User is fetched")
+})
+
+app.get("/admin", (req, res) => {
+    res.send("Hello World");
+})
+
+app.get("/admin/getAllData", (req, res)=>{
+    res.send("Data is sent back from admin")
+})
+
+app.get("/admin/deleteAllData", (req, res)=> {
+    res.send("Data is deleted")
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server is successfully listening on port ${PORT}....`);
---
+++ src/middlewares/auth.js
@@ -0,0 +1,14 @@
+const authAdmin = (req, res, next) => {
+    console.log("Admin auth is getting checked !!");
+    const token = "xyz";
+    const isAdminAutherized = token === "xyz";
+    if(!isAdminAutherized) {
+        res.status(401).send("Unauthorised request");
+    } else {
+        next();
+    }
+}
+
+module.exports = {
+    authAdmin,
+}
