7f23a839da5e62ec74e4985c43ae14fb716ad26d
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-01-07 23:07:21 +0530

    missing files

--- src/routes/request.js
+++ src/routes/request.js
@@ -2,6 +2,7 @@ const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require('../middlewares/auth');
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
   try {
@@ -16,6 +17,20 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
       });
     }
 
+    const toUser = await User.findById(toUserId);
+    if (!toUser) {
+      return res.status(400).json({
+        message: "User not found for whome the request is sent",
+      })
+    }
+
+    const sameUser = fromUserId === toUserId;
+    if (!sameUser) {
+      return res.status(400).json({
+        message: "You can not send request to yourself"
+       });
+    }
+
     // we will check if the request is already sent or not
     const existingRequest = await ConnectionRequest.findOne({
       $or: [
@@ -24,7 +39,7 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
       ],
     });
 
-    if(existingRequest){
+    if (existingRequest) {
       return res.status(400).json({
         message: "Request already sent",
       });
