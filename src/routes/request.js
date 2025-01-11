8e1545222975ccb589e57ecac4a5dda0fb4fc191
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-01-11 21:53:25 +0530

    changes for the api

--- src/routes/request.js
+++ src/routes/request.js
@@ -60,4 +60,40 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
   }
 });
 
+requestRouter.post("/request/review/:status/:requestId", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const status = req.params.status;
+    const requestId = req.params.requestId;
+
+    const allowedStatus = ['accepted', 'rejected'];
+    if (!allowedStatus.includes(status)) {
+      return res.status(400).json({
+        message: "Invalid status",
+      });
+    }
+
+    const connectionRequest = await  ConnectionRequest.findOne({_id: requestId, toUserId: loggedInUser._id, status: "interested"});
+
+    if(!connectionRequest){
+      return res.status(400).json({
+        message: "This connection is not found"
+      })
+    } 
+
+    connectionRequest.status = status;
+
+    const data = await connectionRequest.save();
+    res.status(200).json({
+      message : `Connection is ${status}`,
+      data : data
+    })
+
+  } catch (error) {
+    res.status(400).send("Error :: " + error.message);
+  }
+
+
+})
+
 module.exports = requestRouter;
