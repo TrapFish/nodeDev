d716409652864072fcbbd4e32de2f1d0aef88bcb
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-01-17 15:28:14 +0530

    renoved unwanted code

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
-  
+
 }));
 // const User = require("./models/user");
 // const { validateSignUpData } = require("./utils/validation");
@@ -29,87 +29,11 @@ const requestRouter = require('./routes/request');
 const userRouter = require('./routes/user');
 
 
-app.use('/',authRouter);
-app.use('/',profileRouter);
-app.use('/',requestRouter);
+app.use('/', authRouter);
+app.use('/', profileRouter);
+app.use('/', requestRouter);
 app.use('/', userRouter)
 
-
-
-
-// app.get("/feed", async (req, res) => {
-//   try {
-//     const users = await User.find();
-//     if (users.length === 0) {
-//       res.status(400).send("No record found");
-//     } else {
-//       res.send(users);
-//     }
-
-//   } catch (error) {
-//     res.status(400).send(error);
-//   }
-
-// });
-
-// app.get("/getUser", async (req, res) => {
-//   try {
-//     let emailId = req.body.emailId;
-//     const users = await User.find({
-//       emailId: emailId
-//     });
-//     if (users.length === 0) {
-//       res.status(400).send("No record found");
-//     } else {
-//       res.send(users);
-//     }
-
-//   } catch (error) {
-//     res.status(400).send(error);
-//   }
-// })
-
-// app.delete("/user", async (req, res) => {
-//   const userId = req.body.userId;
-//   console.log("Delete", userId);
-//   try {
-//     const user = await User.findByIdAndDelete(userId);
-//     res.send("User deleted successfully")
-//   } catch (error) {
-//     res.status(400).send(error);
-//   }
-// })
-
-// app.patch("/user/:userId", async (req, res) => {
-//   const userId = req.params?.userId;
-//   const data = req.body;
-
-
-//   try {
-//     // await User.findByIdAndUpdate(userId, {emailId: data.emailId});
-//     const ALLOWED_UPDATES = [
-//       "photoUrl", "about", "gender", "age", "skills"
-//     ]
-
-//     const isUpdateAllowed = Object.keys(data).every(k => ALLOWED_UPDATES.includes(k));
-
-//     if (!isUpdateAllowed) {
-//       throw new Error("Update Not allowed");
-//     }
-
-//     if (data?.skills.length > 10) {
-//       throw new Error("Skills cann't be more than 10");
-//     }
-
-//     await User.findByIdAndUpdate(userId, data, {
-//       returnDocument: "after",
-//       runValidators: true,
-//     });
-//     res.send("Data is updated for the user")
-//   } catch (error) {
-//     res.status(400).send(error.message);
-//   }
-// })
 
 connectDb().then(async (connection) => {
   console.log("Data Base is connected successfully");
