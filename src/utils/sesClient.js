2933217cad54334307485f542f584f8689b20005
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-05-21 18:40:05 +0530

    changes for the value

--- src/utils/sesClient.js
+++ src/utils/sesClient.js
@@ -1,6 +1,5 @@
 const { SESClient } = require("@aws-sdk/client-ses");
 // Set the AWS Region.
-console.log("SES Client Region: ", process.env.REGION, process.env.SES_AWS_ACCESS_KEY, process.env.SES_AWS_SECRET_KEY);
 const REGION = process.env.REGION ;
 // Create SES service object.
 const sesClient = new SESClient({ region: REGION, credentials: {
